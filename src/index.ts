178c5a2045cb560d66a814f615365508b6c7905f
Jonas Strassel <info@jonas-strassel.de>
2026-03-05 19:45:53 +0100

    Remove chain param from status call to debug empty response

--- src/index.ts
+++ src/index.ts
@@ -128,9 +128,9 @@ new Elysia()
     "/:chainId/stats",
     async ({ params, status }) => {
       // biome-ignore lint/style/noNonNullAssertion: that is how we defined the type of CHAIN_CONFIG
-      const chain = CHAIN_CONFIG[params.chainId]!.name;
+      const _chain = CHAIN_CONFIG[params.chainId]!.name;
       const { data, error } = await trueblocks.GET("/status", {
-        params: { query: { chain, modes: ["some"], caches: true } },
+        params: { query: { modes: ["some"], caches: true } },
       });
       if (error) return status(502, JSON.stringify(error));
       console.log("status response:", JSON.stringify(data?.data));
